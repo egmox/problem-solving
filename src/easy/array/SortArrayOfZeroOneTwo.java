ca97920c413b2a4930bdfe67c601e84ff8a8c0ab
GUlshan <egmox2@gmail.com>
2021-03-19 02:21:07 +0530

    added sorting of 0, 1, 2

---
+++ src/easy/array/SortArrayOfZeroOneTwo.java
@@ -0,0 +1,36 @@
+package easy.array;
+
+import java.util.Arrays;
+
+class SortArrayOfZeroOneTwo {
+
+	public static void main(String[] args) {
+		int arr[] = { 1, 2, 0, 2, 1, 1, 0, 1, 2 };
+		sort(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+
+	public static void sort(int arr[]) {
+		int med = 0, lo = 0, temp = 0, hi = arr.length - 1;
+		while (med <= hi) {
+			switch (arr[med]) {
+			case 0:
+				temp = arr[lo];
+				arr[lo] = arr[med];
+				arr[med] = temp;
+				++med;
+				++lo;
+				break;
+			case 1:
+				++med;
+				break;
+			case 2:
+				temp = arr[hi];
+				arr[hi] = arr[med];
+				arr[med] = temp;
+				--hi;
+			}
+		}
+	}
+
+}
